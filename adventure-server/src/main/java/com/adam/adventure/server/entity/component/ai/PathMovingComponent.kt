e51cd0fcf4d2b91d2f889bc09c1a854945e60578
Adam Higginson <adam.higginson555@gmail.com>
2019-10-08 12:50:46 +0100

    Improving POMs to build uber jars

--- adventure-server/src/main/java/com/adam/adventure/server/entity/component/ai/PathMovingComponent.kt
+++ adventure-server/src/main/java/com/adam/adventure/server/entity/component/ai/PathMovingComponent.kt
@@ -2,9 +2,11 @@ package com.adam.adventure.server.entity.component.ai
 
 import com.adam.adventure.entity.EntityComponent
 import com.adam.adventure.entity.component.event.ComponentEvent
+import com.adam.adventure.entity.component.event.MovementComponentEvent
 import com.adam.adventure.entity.component.tilemap.TilemapComponent
 import com.adam.adventure.scene.SceneManager
 import org.joml.Vector3f
+import java.lang.Math.*
 import java.util.*
 import javax.inject.Inject
 
@@ -12,6 +14,13 @@ import javax.inject.Inject
  * Class which moves an entity along a given path
  */
 class PathMovingComponent(private val speed : Float) : EntityComponent() {
+
+    private enum class DirectionAnimation(val animationName: MovementComponentEvent.MovementType) {
+        EAST(MovementComponentEvent.MovementType.ENTITY_MOVE_EAST),
+        NORTH(MovementComponentEvent.MovementType.ENTITY_MOVE_NORTH),
+        WEST(MovementComponentEvent.MovementType.ENTITY_MOVE_WEST),
+        SOUTH(MovementComponentEvent.MovementType.ENTITY_MOVE_SOUTH)
+    }
 
     @Inject
     private lateinit var sceneManager : SceneManager
@@ -62,7 +71,17 @@ class PathMovingComponent(private val speed : Float) : EntityComponent() {
 
             val direction = targetPosition.sub(entityTranslation).normalize()
             transformComponent.transform.translate(direction.x * speed * deltaTime, direction.y * speed * deltaTime, 0.0f)
+
+            calculateAnimation(direction)
         }
+    }
+
+    private fun calculateAnimation(direction : Vector3f) {
+
+        val angle = atan2(direction.y.toDouble(), direction.x.toDouble())
+        val quadrant = round(4 * angle / (2*PI) + 4) % 4
+        val animation = DirectionAnimation.values()[quadrant.toInt()].animationName
+        broadcastComponentEvent(MovementComponentEvent(animation))
     }
 
     private fun popStackToCurrentNode() {
